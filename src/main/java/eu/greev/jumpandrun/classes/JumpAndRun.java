ec34e52df08c1d5a8b05b96bf165645a4846430b
kevin1722000 <kevin1722000@gmail.com>
2019-12-25 00:07:36 +0100

    placing blocks and teleporting player

--- src/main/java/eu/greev/jumpandrun/classes/JumpAndRun.java
+++ src/main/java/eu/greev/jumpandrun/classes/JumpAndRun.java
@@ -1,6 +1,8 @@
 package eu.greev.jumpandrun.classes;
 
 import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
@@ -14,26 +16,67 @@ public class JumpAndRun {
     private Location startLocation;
     private Location endLocation;
 
+    private byte colorCode;
+
     public JumpAndRun(Player player, Location startLocation) {
         this.player = player;
         this.startLocation = startLocation;
+        this.colorCode = (byte) Maths.randInt(0, 15);
         generateEndLocation();
 
         jumpAndRuns.add(this);
 
         placeStartBlock();
         placeEndBlock();
+
+        player.teleport(this.startLocation.add(0.5, 1, 0.5));
     }
 
     public void placeStartBlock() {
+        Block block = this.startLocation.getBlock();
+        block.setType(Material.STAINED_CLAY);
+        block.setData(this.colorCode);
     }
 
     public void placeEndBlock() {
+        Block block = this.startLocation.getBlock();
+        block.setType(Material.WOOL);
+        block.setData(this.colorCode);
     }
 
     public void generateEndLocation() {
-        this.endLocation = this.startLocation;
-        this.endLocation.setX(this.endLocation.getBlockY() + 5);
+        int maxLength = 5;
+        int yAdd = Maths.randInt(0, 1);
+        int offset = Maths.randInt(0, 3);
+        int xAdd;
+        int zAdd;
+
+        if (yAdd == 1) {
+            maxLength--;
+        }
+        if (offset/2 == 1) {
+            maxLength--;
+        }
+
+        if (Math.random() < 0.5) {
+            xAdd = maxLength;
+            zAdd = offset;
+        } else {
+            xAdd = offset;
+            zAdd = maxLength;
+        }
+        if (Math.random() < 0.5) {
+            xAdd *= -1;
+        }
+        if (Math.random() < 0.5) {
+            zAdd *= -1;
+        }
+
+        this.endLocation = this.startLocation.add(
+            xAdd,
+            yAdd,
+            zAdd
+        );
     }
 
     public Player getPlayer() {
