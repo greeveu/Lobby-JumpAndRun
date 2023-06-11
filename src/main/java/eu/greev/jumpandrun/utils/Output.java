2908d168d855fb559bfb24b3175f7583c5822aad
IDragonRiderI <IDragonRiderI@nyron.net>
2023-06-11 22:22:40 +0200

    Avoid deserialization of string to get IChatBaseComponent

--- src/main/java/eu/greev/jumpandrun/utils/Output.java
+++ src/main/java/eu/greev/jumpandrun/utils/Output.java
@@ -2,6 +2,7 @@ package eu.greev.jumpandrun.utils;
 
 import org.bukkit.entity.Player;
 
+import net.minecraft.server.v1_8_R3.ChatMessage;
 import net.minecraft.server.v1_8_R3.IChatBaseComponent;
 import net.minecraft.server.v1_8_R3.PacketPlayOutChat;
 import net.minecraft.server.v1_8_R3.PacketPlayOutTitle;
@@ -13,14 +14,14 @@ public class Output {
     }
 
     public static void sendActionBar(Player p, String msg) {
-        IChatBaseComponent cbc = IChatBaseComponent.ChatSerializer.a("{\"text\": \"" + msg + "\"}");
+        IChatBaseComponent cbc = new ChatMessage(msg);
         PacketPlayOutChat ppoc = new PacketPlayOutChat(cbc, (byte) 2);
         ((CraftPlayer) p).getHandle().playerConnection.sendPacket(ppoc);
     }
 
     public static void sendTitle(Player p, String text, String sbtext) {
-        IChatBaseComponent chatTitle = IChatBaseComponent.ChatSerializer.a("{text:\"§6" + text + "\"}");
-        IChatBaseComponent chatSubtitle = IChatBaseComponent.ChatSerializer.a("{text:\"" + sbtext + "\"}");
+        IChatBaseComponent chatTitle = new ChatMessage("§6" + text);
+        IChatBaseComponent chatSubtitle = new ChatMessage(sbtext);
         PacketPlayOutTitle packetPlayOutTimes = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.TIMES, null, 10, 60, 10);
         PacketPlayOutTitle packetPlayOutTitle = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.TITLE, chatTitle);
         PacketPlayOutTitle packetPlayOutSubtitle = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.SUBTITLE, chatSubtitle);
